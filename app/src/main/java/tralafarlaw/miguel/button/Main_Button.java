329f729cf01eea86414c14f6f3a4de174fa23cd6
TheProdigyK <luisale0410@gmail.com>
2018-12-11 13:43:19 -0400

    Prueba, cambie el titulo del proyecto

--- app/src/main/java/tralafarlaw/miguel/button/Main_Button.java
+++ app/src/main/java/tralafarlaw/miguel/button/Main_Button.java
@@ -9,5 +9,6 @@ public class Main_Button extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__button);
+        //HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     }
 }
