aa9acf52d1cf0c1c5f2f2ee7744238e748e9ace0
TheProdigyK <luisale0410@gmail.com>
2018-12-13 13:08:33 -0400

    me olvide una linea en el main :'v

--- app/src/main/java/tralafarlaw/miguel/button/MainActivity.java
+++ app/src/main/java/tralafarlaw/miguel/button/MainActivity.java
@@ -124,6 +124,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        checkPermissions();
 
     }
 
